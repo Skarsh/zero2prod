7ab06d5d7aae786206be8e8ad4b68681b9bd8cbc
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2022-03-29 13:04:28 -0700

    Fix integration test, working on subscriptions endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 //! main.rs
 
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();
+
+    let address = format!("http://127.0.0.1:{}", port);
+
+    println!("App running on {}", address);
+
+    run(listener)?.await
 }
